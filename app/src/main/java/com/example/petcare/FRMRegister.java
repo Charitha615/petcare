9e8dda8bf5053cd40c5254e3a8190f82373a5445
Charitha Wickramarathna <56013746+Charitha615@users.noreply.github.com>
2024-01-02 23:10:46 +0530

    initial project

---
+++ app/src/main/java/com/example/petcare/FRMRegister.java
@@ -0,0 +1,112 @@
+package com.example.petcare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.UUID;
+import java.util.regex.Pattern;
+
+public class FRMRegister extends AppCompatActivity {
+
+    TextInputEditText txtName, txtUserName,txtemail,txtPhoneNumber,txtpassword;
+    Button btnReg;
+
+    FirebaseDatabase database;
+    DatabaseReference reference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frmregister);
+
+        txtName = findViewById(R.id.txtName);
+        txtUserName = findViewById(R.id.txtUserName);
+        txtemail = findViewById(R.id.txtemail);
+        txtPhoneNumber = findViewById(R.id.txtPhoneNumber);
+        txtpassword = findViewById(R.id.txtpassword);
+        btnReg = findViewById(R.id.btnReg);
+
+        btnReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Validate input fields
+                if (validateInputs()) {
+                    // Proceed with user registration
+                    database = FirebaseDatabase.getInstance();
+                    reference = database.getReference();
+
+                    String name = txtName.getText().toString();
+                    String email = txtemail.getText().toString();
+                    String username = txtUserName.getText().toString();
+                    String password = txtpassword.getText().toString();
+                    String PhoneNumber = txtPhoneNumber.getText().toString();
+
+                    Log.d("LoginActivity", "Checking username: " + username);
+                    Log.d("LoginActivity", "Checking password: " + password);
+
+                    String randomUserId = UUID.randomUUID().toString();
+
+                    HelperClass helperClass = new HelperClass(name,email,PhoneNumber,username,password);
+                    reference.child("users").child("user_registration").child(randomUserId).setValue(helperClass);
+
+                    Toast.makeText(FRMRegister.this, "You have signed up successfully!", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(FRMRegister.this, LoginFRM.class);
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+
+    private boolean validateInputs() {
+        if (TextUtils.isEmpty(txtName.getText().toString())) {
+            txtName.setError("Please enter your name");
+            return false;
+        } else if (!isAlpha(txtName.getText().toString())) {
+            txtName.setError("Name should contain only alphabetic characters");
+            return false;
+        }
+
+        if (TextUtils.isEmpty(txtemail.getText().toString())) {
+            txtemail.setError("Please enter your email");
+            return false;
+        } else if (!isValidEmail(txtemail.getText().toString())) {
+            txtemail.setError("Please enter a valid email address");
+            return false;
+        }
+
+        if (TextUtils.isEmpty(txtUserName.getText().toString())) {
+            txtUserName.setError("Please enter your username");
+            return false;
+        }
+
+        if (TextUtils.isEmpty(txtpassword.getText().toString())) {
+            txtpassword.setError("Please enter your password");
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean isValidEmail(String email) {
+        // Add your email validation logic here, for example using a regular expression
+        String emailRegex = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z|a-z]{2,}$";
+        Pattern pattern = Pattern.compile(emailRegex);
+        return pattern.matcher(email).matches();
+    }
+
+    private boolean isAlpha(String name) {
+        // Check if the string contains only alphabetic characters
+        return name.matches("[a-zA-Z]+");
+    }
+}
---
+++ app/src/main/java/com/example/petcare/LoginFRM.java
@@ -0,0 +1,140 @@
+package com.example.petcare;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class LoginFRM extends AppCompatActivity {
+
+    Button Lbtnregister,LbtnLogin;
+    TextInputEditText LtxtEmail, LtxtPassword;
+
+    public static final String ERROR_EMPTY_USERNAME = "Username cannot be empty";
+    public static final String ERROR_EMPTY_PASSWORD = "Password cannot be empty";
+    public static final String ERROR_INVALID_CREDENTIALS = "Invalid username or password";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_frm);
+
+        Lbtnregister = findViewById(R.id.Lbtnregister);
+        LbtnLogin = findViewById(R.id.LbtnLogin);
+        LtxtEmail = findViewById(R.id.LtxtEmail);
+        LtxtPassword = findViewById(R.id.LtxtPassword);
+
+
+
+        Lbtnregister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginFRM.this, FRMRegister.class);
+                startActivity(intent);
+            }
+        });
+
+        LbtnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String usernameError = validateUsername();
+                String passwordError = validatePassword();
+
+                if (usernameError != null) {
+                    LtxtEmail.setError(usernameError);
+                }
+
+                if (passwordError != null) {
+                    LtxtPassword.setError(passwordError);
+                }
+
+                if (usernameError == null && passwordError == null) {
+                    // Validation successful, proceed with login
+                    checkUser();
+                }
+            }
+        });
+    }
+    public String validateUsername() {
+        String val = LtxtEmail.getText().toString();
+        if (val.isEmpty()) {
+            return ERROR_EMPTY_USERNAME;
+        } else {
+            return null; // No error
+        }
+    }
+
+    public String validatePassword() {
+        String val = LtxtPassword.getText().toString();
+        if (val.isEmpty()) {
+            return ERROR_EMPTY_PASSWORD;
+        } else {
+            return null; // No error
+        }
+    }
+
+    public void checkUser() {
+        // Initialize Firebase
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference reference = database.getReference("users");
+
+        String userUsername = LtxtEmail.getText().toString().trim();
+        String userPassword = LtxtPassword.getText().toString().trim();
+
+        reference.child("user_registration").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                boolean loginSuccessful = false;
+
+                for (DataSnapshot userSnapshot : dataSnapshot.getChildren()) {
+                    HelperClass user = userSnapshot.getValue(HelperClass.class);
+
+                    if (user != null && user.getEmail().equals(userUsername) && user.getPassword().equals(userPassword)) {
+                        // Login successful
+                        loginSuccessful = true;
+
+                        String name = user.getName();
+                        String email = user.getEmail();
+                        String username = user.getUsername();
+                        String randomUserId = userSnapshot.getKey();
+
+                        SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putString("randomUserId", randomUserId);
+                        editor.apply();
+
+                        // Pass this data to the MainActivity using Intent
+                        Intent intent = new Intent(LoginFRM.this, MainActivity.class);
+                        intent.putExtra("name", name);
+                        intent.putExtra("email", email);
+                        intent.putExtra("username", username);
+                        startActivity(intent);
+                        finish(); // Finish the LoginActivity to prevent going back to it with the back button
+                        break; // Exit the loop since we found a match
+                    }
+                }
+
+                if (!loginSuccessful) {
+                    // Display an error message for invalid credentials
+                    LtxtPassword.setError(ERROR_INVALID_CREDENTIALS);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // Handle onCancelled
+            }
+        });
+    }
+}
